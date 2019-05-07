d69568b4011e94039ccec296d4a50386144453fd
한대영 <dyhan@sk.com>
2019-05-07 14:15:26 +0900

    add builder pattern

---
+++ Ch02/Item02_builder_pattern/src/BuilderMain.java
@@ -0,0 +1,7 @@
+public class BuilderMain {
+    public static void main(String[] args) {
+        NutritionFacts cocaCola = new NutritionFacts.Builder(240, 8)
+                .setCalories(100).setSodium(35).setCarbonhydrate(27).build();
+        System.out.println(cocaCola);
+    }
+}
---
+++ Ch02/Item02_builder_pattern/src/NutritionFacts.java
@@ -0,0 +1,68 @@
+public class NutritionFacts {
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbonhydrate;
+
+    public static class Builder {
+        private final int servingSize;
+        private final int servings;
+
+        private int calories = 0;
+        private int fat = 0;
+        private int sodium = 0;
+        private int carbonhydrate = 0;
+
+        public Builder(int servingSize, int servings) {
+            this.servingSize = servingSize;
+            this.servings = servings;
+        }
+
+        public Builder setCalories(int calories) {
+            this.calories = calories;
+            return this;
+        }
+
+        public Builder setFat(int fat) {
+            this.fat = fat;
+            return this;
+        }
+
+        public Builder setSodium(int sodium) {
+            this.sodium = sodium;
+            return this;
+        }
+
+        public Builder setCarbonhydrate(int carbonhydrate) {
+            this.carbonhydrate = carbonhydrate;
+            return this;
+        }
+
+        public NutritionFacts build() {
+            return new NutritionFacts(this);
+        }
+    }
+
+    private NutritionFacts(Builder builder) {
+        servingSize = builder.servingSize;
+        servings = builder.servings;
+        calories = builder.calories;
+        fat = builder.fat;
+        sodium = builder.sodium;
+        carbonhydrate = builder.carbonhydrate;
+    }
+
+    @Override
+    public String toString() {
+        return "NutritionFacts{" +
+                "servingSize=" + servingSize +
+                ", servings=" + servings +
+                ", calories=" + calories +
+                ", fat=" + fat +
+                ", sodium=" + sodium +
+                ", carbonhydrate=" + carbonhydrate +
+                '}';
+    }
+}
